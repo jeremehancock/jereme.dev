1fed25171b415dc19864979633169a7a76afc850
Jereme <michael@jeremehancock.com>
2019-05-27 18:49:59 -0500

    Promoted Mon May 27 18:49:59 CDT 2019

--- status.php
+++ status.php
@@ -1,4 +1,26 @@
-<php
-    echo shell_exec("uptime");
-?>
+<?php
+    $server = $_SERVER['SERVER_ADDR'];
+    echo "IP: " . $server;
 
+    echo "</br>";
+
+    if ($server == '192.168.86.140') {
+        if ($_SERVER['HTTP_HOST'] == "velvethotdog.com:8080") {
+            echo "Node: 1 (Dev Mode)";
+        } else {
+            echo "Node: 1";
+        }
+
+    } else if ($server == '192.168.86.141') {
+        echo "Node: 2";
+    } else if ($server == '192.168.86.142') {
+        echo "Node: 3";
+    }
+    echo "</br>";
+
+    echo "Uptime: " . exec('uptime -p');
+
+    echo "</br>";
+
+    echo exec('/home/pi/scripts/status/temp');
+?>
