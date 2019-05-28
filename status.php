bb4e7cc233a2cc32bb663002ef320abc88359db2
Jereme <michael@jeremehancock.com>
2019-05-27 19:17:25 -0500

    Promoted Mon May 27 19:17:25 CDT 2019

--- status.php
+++ status.php
@@ -1,3 +1,30 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+<script>
+    $(window).ready( function() {
+
+        var time = 5
+
+        setInterval( function() {
+
+            time--;
+
+            $('#time').html(time);
+
+            if (time === 0) {
+
+                location.reload()
+            }
+
+
+        }, 1000 );
+
+    });
+</script>
+
+<span>Reload in: </span><span id="time">5</span><span> seconds</span>
+</br>
+
 <?php
     $server = $_SERVER['SERVER_ADDR'];
     echo "IP: " . $server;
