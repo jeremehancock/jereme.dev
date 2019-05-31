57c90296d7654b0bb9474af5e2898badde0575c9
Jereme <michael@jeremehancock.com>
2019-05-31 08:02:13 -0500

    Promoted Fri May 31 08:02:13 CDT 2019

--- bl-themes/blekathlon-pilab/js/_blank.js
+++
@@ -1,6 +0,0 @@
-var links = document.querySelectorAll( '.page-content a' );  
-for (var i = 0, length = links.length; i < length; i++) {  
-    if (links[i].hostname != window.location.hostname) {
-        links[i].target = '_blank';
-    }
-}
