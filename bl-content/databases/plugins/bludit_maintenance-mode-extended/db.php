e2c7fd572bef4b5326429f412dee13b8c5064e41
Jereme <michael@jeremehancock.com>
2019-06-02 13:37:44 -0500

    Promoted Sun Jun  2 13:37:43 CDT 2019

--- bl-content/databases/plugins/bludit_maintenance-mode-extended/db.php
+++ bl-content/databases/plugins/bludit_maintenance-mode-extended/db.php
@@ -1,2 +1,2 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{"enable":true,"message":"Temporarily down for maintenance.","textcolor":"FFFFFF","image":"","bgcolor":"B3B3B3","position":1}
+{"enable":false,"message":"Temporarily down for maintenance.","textcolor":"FFFFFF","image":"","bgcolor":"B3B3B3","position":1}
