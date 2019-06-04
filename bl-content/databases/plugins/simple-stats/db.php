18a14b67cc09f376ae2d1b79593ce775c97f18f6
Jereme <michael@jeremehancock.com>
2019-06-03 20:37:25 -0500

    Promoted Mon Jun  3 20:37:25 CDT 2019

--- bl-content/databases/plugins/simple-stats/db.php
+++ bl-content/databases/plugins/simple-stats/db.php
@@ -1,2 +1,2 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{"numberOfDays":7,"label":"Visits","excludeAdmins":false,"position":1,"showContentStats":false}
+{"numberOfDays":7,"label":"Visits","excludeAdmins":true,"position":1,"showContentStats":false}
