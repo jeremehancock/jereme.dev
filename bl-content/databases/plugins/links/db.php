45959a5a45193723a06d5f9db2b25e7a8498f1b8
Jereme <michael@jeremehancock.com>
2019-08-25 20:01:46 -0500

    Promoted Sun Aug 25 20:01:46 CDT 2019

--- bl-content/databases/plugins/links/db.php
+++ bl-content/databases/plugins/links/db.php
@@ -1,2 +1,2 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{"label":"Links","jsondb":"{&quot;Random Goat&quot;:&quot;https:\\\/\\\/randomgoat.com&quot;,&quot;Snopes It!&quot;:&quot;https:\\\/\\\/snopesit.com\\\/&quot;,&quot;m7Kodi&quot;:&quot;https:\\\/\\\/m7kodi.dev&quot;,&quot;Jereme&quot;:&quot;https:\\\/\\\/jereme.dev&quot;}","position":"3"}
+{"label":"Links","jsondb":"{&quot;Random Goat&quot;:&quot;https:\\\/\\\/randomgoat.com&quot;,&quot;Dumb Projects&quot;:&quot;https:\\\/\\\/dumbprojects.com&quot;,&quot;m7kodi&quot;:&quot;https:\\\/\\\/m7kodi.dev\\\/&quot;,&quot;Jereme&quot;:&quot;https:\\\/\\\/jereme.dev&quot;}","position":"3"}
