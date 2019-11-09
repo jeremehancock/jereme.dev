0b122d136c4ae4fad80e2f4a7091f1786f939dbb
Jereme Hancock <michael@jeremehancock.com>
2019-11-09 00:05:20 -0600

    Promoted Sat  9 Nov 00:05:20 CST 2019

--- bl-content/databases/security.php
+++ bl-content/databases/security.php
@@ -1,2 +1,2 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{"minutesBlocked":5,"numberFailuresAllowed":10,"blackList":{"192.168.86.128":{"lastFailure":1557402817,"numberFailures":1},"192.168.86.119":{"lastFailure":1557450850,"numberFailures":2}}}
+{"minutesBlocked":5,"numberFailuresAllowed":10,"blackList":{"192.168.86.128":{"lastFailure":1557402817,"numberFailures":1},"192.168.86.119":{"lastFailure":1573279183,"numberFailures":2}}}
