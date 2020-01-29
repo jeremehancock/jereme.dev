b0d96704a1ee2968f447c008bf7e991612c23f8b
Jereme Hancock <michael@jeremehancock.com>
2020-01-28 22:33:08 -0600

    Promoted Tue 28 Jan 22:33:07 CST 2020

--- bl-content/workspaces/backup/2020-01-28-22-31-48/databases/plugins/links/db.php
+++
@@ -1,2 +0,0 @@
-<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{"label":"Links","jsondb":"{&quot;Resum\\u00e9&quot;:&quot;https:\\\/\\\/jeremehancock.com&quot;,&quot;Github&quot;:&quot;https:\\\/\\\/jereme.dev&quot;,&quot;m7kodi&quot;:&quot;https:\\\/\\\/m7kodi.dev&quot;}","position":"3"}
