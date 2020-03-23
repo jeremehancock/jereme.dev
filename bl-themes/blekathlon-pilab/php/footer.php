01c4a06922e6d15675335cad01ffd15034b6e89d
Jereme Hancock <michael@jeremehancock.com>
2020-03-23 18:11:22 -0500

    Promoted Mon 23 Mar 18:11:22 CDT 2020

--- bl-themes/blekathlon-pilab/php/footer.php
+++ bl-themes/blekathlon-pilab/php/footer.php
@@ -4,6 +4,11 @@
             <?php echo $site->footer(); ?>
             <span class="sep"> | </span>
             <?php Theme::plugins('siteFooter'); ?>
+            <label class="dark-toggle">
+                <input id="dark-toggle" class="dark-toggle-checkbox" type="checkbox" onclick="darkmode.toggle();">
+                <div class="dark-toggle-switch"></div>
+                <span class="dark-toggle-label">Dark Mode</span>
+            </label>
         </div>
         <div class="backer">
                 <a href="https://www.patreon.com/bludit">Bludit Backer <img src="../bl-themes/blekathlon-pilab/img/patreon.png" class="backer-logo"/></a>
@@ -12,4 +17,16 @@
 </footer>
 <script>
 	var uploadsFolder = '<?php echo HTML_PATH_UPLOADS; ?>';
+</script>
+
+<script src="../bl-themes/blekathlon-pilab/js/darkmode-js.min.js"></script>
+
+<script>
+const darkmode =  new Darkmode();
+if (darkmode.isActivated()) {
+document.getElementById("dark-toggle").checked = true;
+}
+else {
+document.getElementById("dark-toggle").checked = false;
+}
 </script>
