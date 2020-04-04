1ca7a9594d0b601bc9057a5e71b3a00d10d0bebb
Jereme Hancock <michael@jeremehancock.com>
2020-04-03 21:40:14 -0500

    Promoted Fri  3 Apr 21:40:14 CDT 2020

--- bl-themes/blekathlon-pilab/index.php
+++ bl-themes/blekathlon-pilab/index.php
@@ -21,6 +21,13 @@
 
     <?php echo Theme::javascript('js/bundle.min.js'); ?>
 
+    <script src="../bl-themes/blekathlon-pilab/js/lozad.min.js"></script>
+    
+    <script>
+        const observer = lozad(); // lazy loads elements with default selector as '.lozad'
+        observer.observe();
+    </script>
+
     <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
     <?php Theme::plugins('siteBodyEnd'); ?>
