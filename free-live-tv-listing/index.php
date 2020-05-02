dc9f66329f2ea1bf281a9d7a295fb3ae3ed5c08f
Jereme Hancock <michael@jeremehancock.com>
2020-05-01 19:57:22 -0500

    Promoted Fri  1 May 19:57:22 CDT 2020

---
+++ free-live-tv-listing/index.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="../bl-themes/blekathlon-pilab/css/style.min.css">
+<link rel="stylesheet" type="text/css" href="../bl-themes/blekathlon-pilab/css/pilab.css">
+<link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700|Roboto:400,400i,700&display=swap" rel="stylesheet" />
+</head>
+<body>
+<div class="table-border1"	>
+<div class="table-wrapper" style="height: 100%">
+<table>
+    <thead>
+    <tr>
+        <th>Channel</th>
+        <th>Genre</th>
+    </tr>
+    </thead>
+    <tbody>
+<?php
+
+function file_get_contents_curl($url) {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT , 30);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+}
+
+function get_json($url) {
+    $source = file_get_contents_curl($url);
+    return json_decode($source, true);
+}
+
+$api = "https://m7lib.dev/api/v1/channels/";
+$json = get_json($api);
+
+foreach ($json as $key => $values) {
+	echo '<tr>';
+	echo "<td>$values[name]</td>";
+	echo "<td>$values[genre]</td>";
+	echo '</tr>';
+}
+	
+?>
+    </tbody>
+</table>
+</div>
+</div>
