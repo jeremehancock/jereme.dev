086f0ca48f6ea16ea6340cddbcf4c98932996877
Jereme Hancock <michael@jeremehancock.com>
2020-06-01 21:55:59 -0500

    Promoted Mon  1 Jun 21:55:59 CDT 2020

--- bl-themes/blekathlon-pilab/index.php
+++ bl-themes/blekathlon-pilab/index.php
@@ -27,7 +27,7 @@
         observer.observe();
     </script>
 
-    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    <!--<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>-->
 
     <?php Theme::plugins('siteBodyEnd'); ?>
 
