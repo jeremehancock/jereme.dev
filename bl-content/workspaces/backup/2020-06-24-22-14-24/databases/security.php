32d0801958aed337dcf96b175ed253043d3cb7ef
Jereme Hancock <michael@jeremehancock.com>
2020-06-26 14:21:41 -0500

    Promoted Fri 26 Jun 14:21:41 CDT 2020

--- bl-content/workspaces/backup/2020-06-24-22-14-24/databases/security.php
+++
@@ -1,2 +0,0 @@
-<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{"minutesBlocked":5,"numberFailuresAllowed":10,"blackList":{"192.168.86.128":{"lastFailure":1557402817,"numberFailures":1},"192.168.86.119":{"lastFailure":1585619927,"numberFailures":1}}}
