727acb0435a5bf96d7405dcc97bf87e16fb3f943
Jereme Hancock <michael@jeremehancock.com>
2020-08-12 11:00:49 -0500

    Promoted Wed 12 Aug 11:00:49 CDT 2020

--- bl-content/databases/plugins/pilab-navigation/db.php
+++ bl-content/databases/plugins/pilab-navigation/db.php
@@ -1,2 +1,2 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{"label":"Latest Posts","homeLink":false,"numberOfItems":4,"position":1}
+{"label":"Latest Posts","homeLink":false,"numberOfItems":3,"position":1}
