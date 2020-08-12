59d09ed90554fd52c18291c44947784811361f06
Jereme Hancock <michael@jeremehancock.com>
2020-08-11 21:55:11 -0500

    Promoted Tue 11 Aug 21:55:11 CDT 2020

--- restreamer-admin/index.php
+++ restreamer-admin/index.php
@@ -1,5 +1,6 @@
 <html>
 	<head>
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	    <title>Restreamer Admin</title>
 		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
