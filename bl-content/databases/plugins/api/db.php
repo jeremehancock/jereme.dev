bb3c96528d85cc8eca18fc951782d3463295749d
Jereme Hancock <michael@jeremehancock.com>
2021-05-16 17:08:51 -0500

    Promoted Sun 16 May 17:08:51 CDT 2021

--- bl-content/databases/plugins/api/db.php
+++ bl-content/databases/plugins/api/db.php
@@ -1,2 +1,2 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{"token":"f77c596ec98f9bc1b764e5e30a86486f","numberOfItems":1000,"position":1}
+{"token":"f77c596ec98f9bc1b764e5e30a86486f","numberOfItems":10,"position":1}
