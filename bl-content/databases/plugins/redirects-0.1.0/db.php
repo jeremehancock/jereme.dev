34e4a6e8251f38c53f3b57aabbf8edad5d6b4da3
Jereme Hancock <michael@jeremehancock.com>
2022-10-18 08:41:18 -0500

    Promoted Tue 18 Oct 08:41:18 CDT 2022

--- bl-content/databases/plugins/redirects-0.1.0/db.php
+++ bl-content/databases/plugins/redirects-0.1.0/db.php
@@ -1,2 +1,2 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{"force-https":false,"https-status":301,"allow-external":true,"redirects":{"\/status":["https:\/\/stats.uptimerobot.com\/YMAv7t4gK\/782645566",301],"\/pi-lab-stats":["\/stats",301],"\/welcome-to-pi-lab":["\/specs",301]},"position":1}
+{"force-https":false,"https-status":301,"allow-external":true,"redirects":{"\/status":["https:\/\/status.pilab.dev\/status\/default",301],"\/pi-lab-stats":["\/stats",301],"\/welcome-to-pi-lab":["\/specs",301]},"position":1}
