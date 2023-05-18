a2d7e14513097f787361911e79afe0e40626ebc2
Jereme Hancock <michael@jeremehancock.com>
2023-05-18 07:18:59 -0500

    Promoted Thu 18 May 07:18:59 CDT 2023

--- bl-content/databases/plugins/site-redirect-pilab/db.php
+++ bl-content/databases/plugins/site-redirect-pilab/db.php
@@ -1,2 +1,2 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{"enable":true,"port":"8080","url":"https:\/\/pilab.dev","position":1}
+{"enable":false,"port":"8080","url":"https:\/\/pilab.dev","position":1}
