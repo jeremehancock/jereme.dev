c722ae48634671ba4a12c309b36ea01d11c4e007
Jereme Hancock <michael@jeremehancock.com>
2024-09-25 13:48:04 -0500

    Promoted Wed 25 Sep 13:48:04 CDT 2024

--- bl-content/databases/users.php
+++ bl-content/databases/users.php
@@ -1,2 +1,26 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{"admin":{"nickname":"Admin","firstName":"Jereme","lastName":"Hancock","role":"admin","password":"!","salt":"5cd39e23c584d","email":"bozodev@protonmail.com","registered":"2019-05-08 22:27:31","tokenRemember":"","tokenAuth":"a1d8ed00efccc1fb80f6cfd3f33806d4","tokenAuthTTL":"2009-03-15 14:00","twitter":"","facebook":"","instagram":"","codepen":"","linkedin":"","github":"","gitlab":"","mastodon":"","description":"","vk":""}}
+{
+    "admin": {
+        "nickname": "Admin",
+        "firstName": "Jereme",
+        "lastName": "Hancock",
+        "role": "admin",
+        "password": "5e3ee0455cdbbe4729f00b1333fcf3874558eeab",
+        "salt": "66f45a5a442a3",
+        "email": "bozodev@protonmail.com",
+        "registered": "2019-05-08 22:27:31",
+        "tokenRemember": "",
+        "tokenAuth": "a1d8ed00efccc1fb80f6cfd3f33806d4",
+        "tokenAuthTTL": "2009-03-15 14:00",
+        "twitter": "",
+        "facebook": "",
+        "instagram": "",
+        "codepen": "",
+        "linkedin": "",
+        "github": "",
+        "gitlab": "",
+        "mastodon": "",
+        "description": "",
+        "vk": ""
+    }
+}
