5a87cd1d7b5c42ce05f0162c9c4f1040fd364c34
Jereme Hancock <michael@jeremehancock.com>
2025-02-06 09:56:07 -0600

    Promoted Thu  6 Feb 09:56:07 CST 2025

--- bl-content/databases/syslog.php
+++ bl-content/databases/syslog.php
@@ -1,6 +1,14 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
 [
     {
+        "date": "2025-02-06 09:56:04",
+        "dictionaryKey": "content-edited",
+        "notes": "Libretto: Plex Library Metadata Exporter",
+        "idExecution": "67a4db93e0886",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
         "date": "2025-02-06 09:54:16",
         "dictionaryKey": "plugin-configured",
         "notes": "Links",
@@ -69,14 +77,6 @@
         "dictionaryKey": "content-edited",
         "notes": "AI Game Collection",
         "idExecution": "67588276760de",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2024-12-10 11:23:31",
-        "dictionaryKey": "content-edited",
-        "notes": "MovieCLI",
-        "idExecution": "6758791358f61",
         "method": "POST",
         "username": "admin"
     }
