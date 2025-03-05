999003d7e5ee2d0db8e6fb9120882451b99393cc
Jereme Hancock <michael@jeremehancock.com>
2025-03-05 12:59:29 -0600

    Promoted Wed  5 Mar 12:59:29 CST 2025

--- bl-content/databases/syslog.php
+++ bl-content/databases/syslog.php
@@ -1,82 +1,82 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
 [
     {
+        "date": "2025-03-05 12:59:21",
+        "dictionaryKey": "content-edited",
+        "notes": "Introducing Posteria",
+        "idExecution": "67c89f08eadda",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
+        "date": "2025-03-05 12:59:00",
+        "dictionaryKey": "content-edited",
+        "notes": "Introducing Posteria",
+        "idExecution": "67c89ef431dc0",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
+        "date": "2025-03-05 12:58:40",
+        "dictionaryKey": "content-edited",
+        "notes": "Introducing Posteria",
+        "idExecution": "67c89ee0334bb",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
+        "date": "2025-03-05 12:58:26",
+        "dictionaryKey": "content-edited",
+        "notes": "Introducing Posteria",
+        "idExecution": "67c89ed23d697",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
+        "date": "2025-03-05 12:58:12",
+        "dictionaryKey": "content-edited",
+        "notes": "Introducing Posteria",
+        "idExecution": "67c89ec2f2b75",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
+        "date": "2025-03-05 12:57:47",
+        "dictionaryKey": "content-edited",
+        "notes": "Introducing Posteria",
+        "idExecution": "67c89eab47dbc",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
+        "date": "2025-03-05 12:56:45",
+        "dictionaryKey": "content-edited",
+        "notes": "Introducing Posteria",
+        "idExecution": "67c89e6cb92b0",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
+        "date": "2025-03-05 12:47:29",
+        "dictionaryKey": "content-edited",
+        "notes": "Introducing Posteria",
+        "idExecution": "67c89c40b6044",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
+        "date": "2025-03-05 12:47:03",
+        "dictionaryKey": "content-edited",
+        "notes": "Introducing Posteria",
+        "idExecution": "67c89c26b9995",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
         "date": "2025-02-26 12:38:32",
         "dictionaryKey": "content-edited",
         "notes": "Introducing Posteria",
         "idExecution": "67bf5fa82af2b",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-02-26 09:50:45",
-        "dictionaryKey": "content-edited",
-        "notes": "Introducing Posteria",
-        "idExecution": "67bf3853c315b",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-02-26 09:49:35",
-        "dictionaryKey": "content-edited",
-        "notes": "Introducing Posteria",
-        "idExecution": "67bf380e90fd3",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-02-26 09:46:02",
-        "dictionaryKey": "content-edited",
-        "notes": "Introducing Posteria",
-        "idExecution": "67bf373a26e88",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-02-26 09:44:24",
-        "dictionaryKey": "content-edited",
-        "notes": "Introducing Posteria",
-        "idExecution": "67bf36d836900",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-02-26 09:43:38",
-        "dictionaryKey": "content-edited",
-        "notes": "Introducing Posteria",
-        "idExecution": "67bf36a9d5304",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-02-26 09:42:55",
-        "dictionaryKey": "content-edited",
-        "notes": "Introducing Posteria",
-        "idExecution": "67bf367f25871",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-02-26 09:42:11",
-        "dictionaryKey": "new-content-created",
-        "notes": "Introducing Posteria",
-        "idExecution": "67bf365242767",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-02-26 09:42:10",
-        "dictionaryKey": "content-deleted",
-        "notes": "autosave-782e531ab637451cf79a40ca150604c5",
-        "idExecution": "67bf365242767",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-02-26 09:37:52",
-        "dictionaryKey": "new-content-created",
-        "notes": "Introducing Posteria[Autosave]",
-        "idExecution": "67bf355043b07",
         "method": "POST",
         "username": "admin"
     }
