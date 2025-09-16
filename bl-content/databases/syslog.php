6416d68a82030b774ff3f303dfc68c61d419bba3
Jereme Hancock <michael@jeremehancock.com>
2025-09-16 17:06:15 -0500

    Promoted Tue 16 Sep 17:06:15 CDT 2025

--- bl-content/databases/syslog.php
+++ bl-content/databases/syslog.php
@@ -1,6 +1,22 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
 [
     {
+        "date": "2025-09-16 17:06:10",
+        "dictionaryKey": "content-edited",
+        "notes": "Specs",
+        "idExecution": "68c9df5203ee0",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
+        "date": "2025-09-16 17:06:10",
+        "dictionaryKey": "content-deleted",
+        "notes": "autosave-ae6baedd36be1d5b3fa9ec0d872cc735",
+        "idExecution": "68c9df5203ee0",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
         "date": "2025-09-16 17:01:17",
         "dictionaryKey": "content-edited",
         "notes": "Specs",
@@ -61,22 +77,6 @@
         "dictionaryKey": "content-deleted",
         "notes": "autosave-ae6baedd36be1d5b3fa9ec0d872cc735",
         "idExecution": "68c82a8f3f1b1",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-09-15 10:01:51",
-        "dictionaryKey": "new-content-created",
-        "notes": "Specs",
-        "idExecution": "68c82a5eae246",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-09-15 10:01:48",
-        "dictionaryKey": "content-edited",
-        "notes": "Specs",
-        "idExecution": "68c82a5befd84",
         "method": "POST",
         "username": "admin"
     }
