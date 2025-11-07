15080afda62ddf7d9856c985010d3a9e7dd383d8
Jereme Hancock <michael@jeremehancock.com>
2025-11-06 21:41:28 -0600

    Promoted Thu  6 Nov 21:41:28 CST 2025

--- bl-content/databases/syslog.php
+++ bl-content/databases/syslog.php
@@ -1,6 +1,38 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
 [
     {
+        "date": "2025-11-06 21:41:17",
+        "dictionaryKey": "content-edited",
+        "notes": "Specs",
+        "idExecution": "690d6a5c88798",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
+        "date": "2025-11-06 21:41:16",
+        "dictionaryKey": "content-deleted",
+        "notes": "autosave-ae6baedd36be1d5b3fa9ec0d872cc735",
+        "idExecution": "690d6a5c88798",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
+        "date": "2025-11-06 21:39:58",
+        "dictionaryKey": "content-edited",
+        "notes": "Specs[Autosave]",
+        "idExecution": "690d6a0d91d3a",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
+        "date": "2025-11-06 21:37:58",
+        "dictionaryKey": "new-content-created",
+        "notes": "Specs[Autosave]",
+        "idExecution": "690d6995931d5",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
         "date": "2025-11-05 11:36:08",
         "dictionaryKey": "content-edited",
         "notes": "Specs",
@@ -45,38 +77,6 @@
         "dictionaryKey": "content-edited",
         "notes": "Specs",
         "idExecution": "68c9e0129bf3e",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-09-16 17:06:10",
-        "dictionaryKey": "content-edited",
-        "notes": "Specs",
-        "idExecution": "68c9df5203ee0",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-09-16 17:06:10",
-        "dictionaryKey": "content-deleted",
-        "notes": "autosave-ae6baedd36be1d5b3fa9ec0d872cc735",
-        "idExecution": "68c9df5203ee0",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-09-16 17:01:17",
-        "dictionaryKey": "content-edited",
-        "notes": "Specs",
-        "idExecution": "68c9de2cc9672",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-09-16 17:00:34",
-        "dictionaryKey": "content-edited",
-        "notes": "Specs",
-        "idExecution": "68c9de0201cbd",
         "method": "POST",
         "username": "admin"
     }
