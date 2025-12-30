d87293165f4479398b09a594813c964745a3a418
Jereme Hancock <michael@jeremehancock.com>
2025-12-30 11:42:04 -0600

    Promoted Tue 30 Dec 11:42:04 CST 2025

--- bl-content/databases/syslog.php
+++ bl-content/databases/syslog.php
@@ -1,6 +1,46 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
 [
     {
+        "date": "2025-12-30 11:41:55",
+        "dictionaryKey": "content-edited",
+        "notes": "Specs",
+        "idExecution": "69540ee26d076",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
+        "date": "2025-12-30 11:41:54",
+        "dictionaryKey": "content-deleted",
+        "notes": "autosave-ae6baedd36be1d5b3fa9ec0d872cc735",
+        "idExecution": "69540ee26d076",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
+        "date": "2025-12-30 11:41:20",
+        "dictionaryKey": "content-edited",
+        "notes": "Specs[Autosave]",
+        "idExecution": "69540ec055104",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
+        "date": "2025-12-30 11:39:20",
+        "dictionaryKey": "content-edited",
+        "notes": "Specs[Autosave]",
+        "idExecution": "69540e4855cdc",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
+        "date": "2025-12-30 11:37:21",
+        "dictionaryKey": "new-content-created",
+        "notes": "Specs[Autosave]",
+        "idExecution": "69540dd0983d5",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
         "date": "2025-12-29 14:06:18",
         "dictionaryKey": "plugin-configured",
         "notes": "Links",
@@ -37,46 +77,6 @@
         "dictionaryKey": "content-deleted",
         "notes": "weather-dashboard",
         "idExecution": "6952ded76fc48",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-12-29 14:04:15",
-        "dictionaryKey": "new-content-created",
-        "notes": "Bouncing Trump!",
-        "idExecution": "6952debee1123",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-12-29 14:03:56",
-        "dictionaryKey": "new-content-created",
-        "notes": "Weather Dashboard",
-        "idExecution": "6952deab909a6",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-12-27 11:13:12",
-        "dictionaryKey": "content-edited",
-        "notes": "Specs",
-        "idExecution": "695013a826b2b",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-12-27 11:11:37",
-        "dictionaryKey": "content-edited",
-        "notes": "Specs",
-        "idExecution": "695013492ed34",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2025-12-27 11:11:06",
-        "dictionaryKey": "content-edited",
-        "notes": "Specs",
-        "idExecution": "69501329e76b6",
         "method": "POST",
         "username": "admin"
     }
