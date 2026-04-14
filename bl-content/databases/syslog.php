f1a302df3a65430037fdd37833405d37d98f16df
Jereme Hancock <michael@jeremehancock.com>
2026-04-14 11:47:27 -0500

    Promoted Tue 14 Apr 11:47:27 CDT 2026

--- bl-content/databases/syslog.php
+++ bl-content/databases/syslog.php
@@ -1,6 +1,22 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
 [
     {
+        "date": "2026-04-14 11:47:12",
+        "dictionaryKey": "content-edited",
+        "notes": "Pi Lab Setup",
+        "idExecution": "69de6f8fa6026",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
+        "date": "2026-04-14 11:46:42",
+        "dictionaryKey": "content-edited",
+        "notes": "Pi Lab Setup",
+        "idExecution": "69de6f71bfa0c",
+        "method": "POST",
+        "username": "admin"
+    },
+    {
         "date": "2026-04-14 10:35:30",
         "dictionaryKey": "plugin-deactivated",
         "notes": "Site Redirect - Pi Lab",
@@ -61,22 +77,6 @@
         "dictionaryKey": "plugin-configured",
         "notes": "Pi Lab - Navigation",
         "idExecution": "69de5b5a6d6dd",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2026-04-14 10:04:11",
-        "dictionaryKey": "plugin-configured",
-        "notes": "Show Node - Pi Lab",
-        "idExecution": "69de576b645fe",
-        "method": "POST",
-        "username": "admin"
-    },
-    {
-        "date": "2026-04-14 10:04:09",
-        "dictionaryKey": "plugin-configured",
-        "notes": "Show Node - Pi Lab",
-        "idExecution": "69de57698ccea",
         "method": "POST",
         "username": "admin"
     }
