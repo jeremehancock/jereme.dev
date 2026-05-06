1d1eab15a7ebe28b2f03085c78eb00f74d4a5b7a
Jereme <michael@jeremehancock.com>
2026-05-06 12:59:24 -0500

    updated links

--- bl-content/databases/plugins/links/db.php
+++ bl-content/databases/plugins/links/db.php
@@ -1,6 +1,2 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{
-    "label": "Links",
-    "jsondb": "{&quot;Resum\\u00e9&quot;:&quot;https:\\\/\\\/jeremehancock.com&quot;,&quot;Ooglester&quot;:&quot;https:\\\/\\\/ooglester.com\\\/&quot;,&quot;Dumb Projects&quot;:&quot;https:\\\/\\\/dumbprojects.com&quot;,&quot;Tortoise Cam&quot;:&quot;https:\\\/\\\/tortoisecam.com\\\/&quot;}",
-    "position": "3"
-}
+{"label":"Links","jsondb":"{&quot;Resum\\u00e9&quot;:&quot;https:\\\/\\\/jeremehancock.com&quot;,&quot;Ooglester&quot;:&quot;https:\\\/\\\/ooglester.com\\\/&quot;,&quot;Dumb Projects&quot;:&quot;https:\\\/\\\/dumbprojects.com&quot;}","position":"3"}
