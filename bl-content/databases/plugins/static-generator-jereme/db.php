600262e6cc1867212df62bb5eb3706b58c62d1e5
Jereme <michael@jeremehancock.com>
2026-05-14 18:28:00 -0500

    updated static build

--- bl-content/databases/plugins/static-generator-jereme/db.php
+++ bl-content/databases/plugins/static-generator-jereme/db.php
@@ -1,2 +1,2 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{"excludePaths":"\/homelab","lastBuildTime":"2026-05-14 18:16:33","lastBuildResult":"ok","lastBuildMessage":"Fetched 374 URLs (0 errors)","lastBuildUrls":374,"lastBuildBytes":57789892,"lastBuildDuration":42.340786933898926,"position":1}
+{"excludePaths":"\/homelab","lastBuildTime":"2026-05-14 18:26:59","lastBuildResult":"ok","lastBuildMessage":"Fetched 374 URLs (0 errors)","lastBuildUrls":374,"lastBuildBytes":57789892,"lastBuildDuration":34.367363929748535,"position":1}
